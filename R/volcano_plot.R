8d2d601f3da6341cb0bf479b830363acd9cd73e7
Saul Pierotti <saulpierotti@gmail.com>
2026-06-05 14:12:00 +0200

    volcano plots work

--- R/volcano_plot.R
+++ R/volcano_plot.R
@@ -13,6 +13,11 @@
 #' @param siamcat object of class \link{siamcat-class}
 #'
 #' @param alpha float, override significance threshold from siamcat object
+#' 
+#' @param effect.metric string, effect metric to use for the x-axis.
+#' If \code{NULL}, the default will be "beta" for linear models and "rank.biserial" for Wilcoxon tests.
+#' Valid options for classification tasks are "fc.log10", "beta" (only for linear models), "rank.biserial", and "auc".
+#' Valid options for regression tasks are "beta", "spearman".
 #'
 #' @param fn.plot string, filename for the plot (any extension supported by
 #' ggsave is allowed). If \code{fn.plot} is \code{NULL}, the plot will only
@@ -28,9 +33,6 @@
 #'
 #' @param annot.y.exp float, percent vertical expansion of the plot area
 #' to accommodate annotations
-#'
-#' @param annot.y.shift float, upward shift of the annotations from the
-#' respective datapoints
 #'
 #' @param font.size integer, base font size for the plot
 #'
@@ -49,10 +51,9 @@
 #' # Simple example
 #' volcano.plot(siamcat_example, fn.plot = "./volcano.pdf")
 volcano.plot <- function(
-    siamcat, alpha=NULL,fn.plot = NULL,
+    siamcat, alpha = NULL, effect.metric = NULL, fn.plot = NULL,
     color.scheme = c("red", "blue", "gray"), annotate = 3,
-    annot.size = 4, annot.y.exp = 0.2, annot.y.shift = NULL,
-    font.size = 14
+    annot.size = 4, annot.y.exp = 0.2, font.size = 14
 ) {
     associations <- associations(siamcat, verbose = 0)
     if (is.null(associations)) {
@@ -65,70 +66,113 @@ volcano.plot <- function(
     assoc.param <- assoc_param(siamcat)
     assoc.param$alpha <- ifelse(is.null(alpha), assoc.param$alpha, alpha)
 
-    if (length(color.scheme) == 1) {
-        tryCatch(
-            {
-                col <- brewer.pal(3, color.scheme)[c(1, 3, 2)]
-            },
-            error = function(e) {
-                stop(
-                    "color.scheme contains 1 element, so it is interpreted ",
-                    "as a ColorBrewer palette, but the palette name is invalid."
-                )
-            }
-        )
-    } else if (length(color.scheme) == 3) {
-        tryCatch(col2rgb(color.scheme), error = function(e) {
-            stop(
-                "color.scheme contains 3 elements, so it is interpreted ",
-                "as containing individual colors, but the color names are",
-                " invalid."
+    switch(
+        as.character(length(color.scheme)),
+        "1" = {
+            tryCatch(
+                {
+                    col <- brewer.pal(3, color.scheme)[c(1, 3, 2)]
+                },
+                error = function(e) {
+                    stop(
+                        "color.scheme contains 1 element, so it is interpreted ",
+                        "as a ColorBrewer palette, but the palette name is invalid."
+                    )
+                }
             )
-        })
-        col <- color.scheme
-    } else {
-        stop(
-            "color.scheme must contain 3 R color values or",
-            " the name of 1 ColorBrewer palette."
+        }, "3" = {
+            tryCatch(col2rgb(color.scheme), error = function(e) {
+                stop(
+                    "color.scheme contains 3 elements, so it is interpreted ",
+                    "as containing individual colors, but the color names are",
+                    " invalid."
+                )
+            })
+            col <- color.scheme
+        },
+        {
+            stop(
+                "color.scheme must contain 3 R color values or",
+                " the name of 1 ColorBrewer palette."
+            )
+        }
+    )
+
+    if (is.null(effect.metric) && assoc.param$test %in% c("lm", "lmer")) {
+        effect.metric <- "beta"
+    } else if (is.null(effect.metric) && assoc.param$test == "wilcoxon") {
+        effect.metric <- "rank.biserial"
+    } else if (
+        !(
+            (label(siamcat)$type == "BINARY" && assoc.param$test == "wilcoxon" && effect.metric %in% c("fc.log10", "auc", "rank.biserial")) ||
+            (label(siamcat)$type == "BINARY" && assoc.param$test %in% c("lm", "lmer") && effect.metric %in% c("fc.log10", "beta", "auc", "rank.biserial")) ||
+            (label(siamcat)$type == "CONTINUOUS" && effect.metric %in% c("beta", "spearman"))
         )
+    ) {
+        stop(sprintf("effect.metric %s is invalid for label type %s and test %s", effect.metric, label(siamcat)$type, assoc.param$test))
     }
+
+    switch(effect.metric,
+        "fc.log10" = {
+            xlab <- bquote(log[10] ~ "fold change")
+            associations$eff <- associations$fc
+            eff.midpoint <- 0
+        },
+        "beta" = {
+            xlab <- bquote("Effect size (" ~ beta ~ ")")
+            associations$eff <- associations$beta
+            eff.midpoint <- 0
+        },
+        "rank.biserial" = {
+            xlab <- "Rank biserial correlation"
+            associations$eff <- associations$rank.biserial
+            eff.midpoint <- 0
+        },
+        "spearman" = {
+            xlab <- bquote(Spearman~rho)
+            associations$eff <- associations$spearman
+            eff.midpoint <- 0
+        },
+        "auc" = {
+            xlab <- "Area under the ROC curve"
+            associations$eff <- associations$auc
+            eff.midpoint <- 0.5
+        },
+        {stop(sprintf("effect.metric %s is invalid", effect.metric))}
+    )
 
     ns.label <- "n. s."
-    if (label(siamcat)$type == "BINARY") {
-        xlab <- bquote(log[10] ~ "fold change")
-        fill_lab <- "Enrichment"
-        control.label <- names(label(siamcat)$info)[label(siamcat)$info == -1]
-        case.label <- names(label(siamcat)$info)[label(siamcat)$info == 1]
-        names(col) <- c(case.label, control.label, ns.label)
-        associations$class <- ifelse(
-            associations$p.adj < assoc.param$alpha,
-            ifelse(associations$fc > 0, case.label, control.label),
-            ns.label
-        )
-        associations$eff <- associations$fc
-    }
+    switch(label(siamcat)$type,
+        "CONTINUOUS" = {
+            fill_lab <- "Effect direction"
+            associations$class <- ifelse(
+                associations$p.adj < assoc.param$alpha,
+                ifelse(associations$eff > eff.midpoint, "Positive", "Negative"),
+                ns.label
+            )
+            names(col) <- c("Positive", "Negative", ns.label)
+        },
+        "BINARY" = {
+            fill_lab <- "Enrichment"
+            control.label <- names(label(siamcat)$info)[label(siamcat)$info == -1]
+            case.label <- names(label(siamcat)$info)[label(siamcat)$info == 1]
+            names(col) <- c(case.label, control.label, ns.label)
+            associations$class <- ifelse(
+                associations$p.adj < assoc.param$alpha,
+                ifelse(associations$eff > eff.midpoint, case.label, control.label),
+                ns.label
+            )
+        },
+        {
+            stop("label type is invalid. Please raise an issue with the package developers.")
+        }
+    )
 
-    if (label(siamcat)$type == "CONTINUOUS") {
-        xlab <- "Effect size"
-        fill_lab <- "Effect direction"
-        names(col) <- c("Positive", "Negative", "n. s.")
-        associations$class <- ifelse(
-            associations$p.adj < assoc.param$alpha,
-            ifelse(associations$beta > 0, "Positive", "Negative"),
-            ns.label
-        )
-        associations$eff <- associations$beta
-    }
-
-    associations.to.label <- associations[
-        associations$p.adj < assoc.param$alpha,
-    ]
-    associations.to.label <- associations.to.label[
-        order(associations.to.label$p.adj),
-    ]
+    associations.to.label <- associations[associations$p.adj < assoc.param$alpha,]
+    associations.to.label <- associations.to.label[order(associations.to.label$p.adj),]
     associations.to.label <- rbind(
-        head(associations.to.label[associations.to.label$eff > 0, ], annotate),
-        head(associations.to.label[associations.to.label$eff < 0, ], annotate)
+        head(associations.to.label[associations.to.label$eff > eff.midpoint, ], annotate),
+        head(associations.to.label[associations.to.label$eff < eff.midpoint, ], annotate)
     )
 
     mult.corr.label <- c(
@@ -192,7 +236,7 @@ volcano.plot <- function(
     # save the plot
     if (!is.null(fn.plot)) {
         ggsave(
-            fn.plot, plot, bg = "white", height = 6, width = 7, device = "png"
+            fn.plot, plot, bg = "white", height = 6, width = 7
         )
     }
 
